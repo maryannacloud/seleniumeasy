e4ab16d37a637bc24f1e7e5961e6b4e37252718c
Maryna Nesterenko <marynanesterenko.qa@gmail.com>
2025-10-21 21:45:41 -0400

    Defining FormButton UI Model

---
+++ src/test/java/seleniumeasy/pageobjects/FormButton.java
@@ -0,0 +1,11 @@
+package seleniumeasy.pageobjects;
+
+import org.openqa.selenium.By;
+
+public class FormButton {
+
+    public static By withLabel(String label) {
+        return By.xpath("//button[text()='" + label + "']");
+
+    }
+}
