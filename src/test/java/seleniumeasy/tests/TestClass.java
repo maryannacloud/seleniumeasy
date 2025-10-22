c2ac1eda0166aeb83a531f5915770fd56c15a8e6
Maryna Nesterenko <marynanesterenko.qa@gmail.com>
2025-10-21 22:15:43 -0400

    Adding tests

--- src/test/java/seleniumeasy/tests/TestClass.java
+++ src/test/java/seleniumeasy/tests/TestClass.java
@@ -3,6 +3,7 @@ package seleniumeasy.tests;
 import net.thucydides.core.annotations.Managed;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
+import seleniumeasy.pageobjects.CheckboxForm;
 import seleniumeasy.pageobjects.MultipleInputForm;
 import seleniumeasy.pageobjects.SingleInputForm;
 
@@ -31,6 +32,33 @@ public class TestClass {
         multipleInputFieldsForm.enterValue2("3");
         multipleInputFieldsForm.getTotal();
         assertThat(multipleInputFieldsForm.displayedTotal()).isEqualTo("5");
+    }
+
+    CheckboxForm checkboxForm;
+
+    @Test
+    public void singleCheckbox() {
+        checkboxForm.open();
+        checkboxForm.setAgeSelected();
+        assertThat(checkboxForm.ageText()).isEqualTo("Success - Check box is checked");
+
+    }
+
+    @Test
+    public void multipleCheckbox() {
+        checkboxForm.open();
+        assertThat(checkboxForm.optionIsCheckedFor("Option 1")).isFalse();
+        assertThat(checkboxForm.optionIsCheckedFor("Option 2")).isFalse();
+        assertThat(checkboxForm.optionIsCheckedFor("Option 3")).isFalse();
+        assertThat(checkboxForm.optionIsCheckedFor("Option 4")).isFalse();
+
+        checkboxForm.clickCheckAllButton();
+
+        assertThat(checkboxForm.optionIsCheckedFor("Option 1")).isTrue();
+        assertThat(checkboxForm.optionIsCheckedFor("Option 2")).isTrue();
+        assertThat(checkboxForm.optionIsCheckedFor("Option 3")).isTrue();
+        assertThat(checkboxForm.optionIsCheckedFor("Option 4")).isTrue();
+
     }
 
 
