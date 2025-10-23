8b4b63cb8135a9872782d92523bf03c4e2950ea8
Maryna Nesterenko <marynanesterenko.qa@gmail.com>
2025-10-22 23:12:10 -0400

    Cleanup

--- src/test/java/seleniumeasy/pageobjects/RadioButtonsForm.java
+++ src/test/java/seleniumeasy/pageobjects/RadioButtonsForm.java
@@ -3,7 +3,6 @@ package seleniumeasy.pageobjects;
 import net.thucydides.core.annotations.DefaultUrl;
 
 @DefaultUrl("http://demo.seleniumeasy.com/basic-checkbox-demo.html")
-
 public class RadioButtonsForm extends SeleniumEasyForm{
 
 
