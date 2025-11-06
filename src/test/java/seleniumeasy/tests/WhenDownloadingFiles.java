605e5c3955400baab938e442ae19ab0d999f3266
Maryna Nesterenko <marynanesterenko.qa@gmail.com>
2025-11-05 20:47:41 -0500

    Fixing annotation imports

--- src/test/java/seleniumeasy/tests/WhenDownloadingFiles.java
+++ src/test/java/seleniumeasy/tests/WhenDownloadingFiles.java
@@ -1,20 +1,21 @@
 package seleniumeasy.tests;
 
-import net.thucydides.core.annotations.Managed;
-import net.thucydides.core.configuration.SessionLocalTempDirectory;
-import org.awaitility.Awaitility;
+import net.serenitybdd.annotations.Managed;
+import net.serenitybdd.junit5.SerenityJUnit5Extension;
+
+import net.thucydides.model.configuration.SessionLocalTempDirectory;
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
 import seleniumeasy.pageobjects.DownloadPage;
 
 import java.io.File;
-import java.nio.file.Paths;
-import java.util.concurrent.TimeUnit;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.awaitility.Awaitility.await;
 
+@ExtendWith(SerenityJUnit5Extension.class)
 public class WhenDownloadingFiles {
 
     @Managed
